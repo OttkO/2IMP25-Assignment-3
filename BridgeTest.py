244bdf8f90dbb59db2642f4e7c986c7bfd0751e0
Nathan Cassee <Nathan Cassee>
2016-03-19 14:10:33 +0100

    Version that shows coreNLP deps in python console

--- BridgeTest.py
+++ BridgeTest.py
@@ -3,19 +3,20 @@ from jpype import *
 import inspect
 
 def main(argv):
-    startJVM("C:\\Program Files (x86)\\Java\\jdk1.8.0_73\\jre\\bin\\server\\jvm.dll", "-ea "
+    startJVM("C:\\Program Files (x86)\\Java\\jre1.8.0_73\\bin\\client\\jvm.dll", "-ea "
                                             ,"-Djava.class.path=Bridge\\target\\stanfordbridge-1.0-jar-with-dependencies.jar")
     java.lang.System.out.println("hello world")
 
-    brPack = JClass("nl.tue.se.bridge.MainBridge")
+#I added this code last night after we finished
 
-    bridge = brPack()
+    brPack = JClass("nl.tue.se.bridge.MainBridge") #finds the class
 
-    print brPack
+    bridge = brPack() #calls the constructor
 
-    print inspect.getmembers(brPack)
-
-
+    print bridge.lineToDependencies("Some random words together that form a sentece that says something.") #calls the method with a random sentence, and gets the output as
+    #a list which it prints
+    #haha awesome, then i will discoard my stuff, h
+    #does it work?
     # and you have to shutdown the VM at the end
     shutdownJVM()
 
--- Dep/install.bat
+++ Dep/install.bat
@@ -6,15 +6,22 @@ SET py=C:\Python27\python.exe
 %py% -m pip uninstall scikit-learn
 %py% -m pip uninstall numpy
 %py% -m pip uninstall nltk
+%py% -m pip uninstall jpype
+%py% -m pip install wheel
 
 rem py -m pip install Dep\scikit_learn-0.17-cp35-none-win32.whl
-%py% -m pip install Dep\scikit_learn-0.15.1-cp27-none-win32.whl
+%py% -m pip install %~dp0\Dep\scikit_learn-0.15.1-cp27-none-win32.whl
 
 rem py -m pip install Dep\numpy-1.10.4+mkl-cp35-cp35m-win32.whl
 rem %py% -m pip install Dep\numpy-1.10.4-cp27-none-win32.whl
-Dep\numpy-1.9.0-win32-superpack-python2.7.exe
+%~dp0\Dep\numpy-1.9.0-win32-superpack-python2.7.exe
 
-Dep\nltk-3.2.win32.exe
-Dep\scipy-0.12.0-win32-superpack-python2.7.exe
+%~dp0\Dep\nltk-3.2.win32.exe
+%~dp0\Dep\scipy-0.12.0-win32-superpack-python2.7.exe
+%~dp0\Dep\JPype-0.5.4.win32-py2.7.exe
 
 %py% -m pip install nose
+
+%py% -c "import nltk; nltk.download(""punkt"")"
+
+pause
